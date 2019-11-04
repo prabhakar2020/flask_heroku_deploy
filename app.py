c74fb3707705cbe1adaf11a5e6e68fd28f643e88
prabhakar <prabhakar2020@gmail.com>
2019-11-04 17:15:04 +0530

    flask application deployment on heroku

--- app.py
+++ app.py
@@ -1,41 +1,42 @@
+from __future__ import print_function
 from flask import Flask, render_template, request, session, redirect, url_for,flash
 app = Flask(__name__)
 
 # @app.route("/hello")
 def home():
     return "Hello world, this is test message"
-class API:
-    def hello(self, revNo=''):
-        print ("*"*100)
-        print (revNo)
-        print ("*"*100)
-        return "hello" +str(revNo), 200
-    def logout(self):
-        session['user'] = ''
-        return redirect(url_for('login'))
-    def login(self):
-        if request.method == 'GET' and session.get('user') == '':
-            return render_template('login.html',data={})
-        else:
-            error = None
-            if request.form.get('name')=='':
-                error = 'invalid user'                
-                return render_template('login.html',error=error)
+# class API:
+#     def hello(self, revNo=''):
+#         print ("*"*100)
+#         print (revNo)
+#         print ("*"*100)
+#         return "hello" +str(revNo), 200
+#     def logout(self):
+#         session['user'] = ''
+#         return redirect(url_for('login'))
+#     def login(self):
+#         if request.method == 'GET' and session.get('user') == '':
+#             return render_template('login.html',data={})
+#         else:
+#             error = None
+#             if request.form.get('name')=='':
+#                 error = 'invalid user'                
+#                 return render_template('login.html',error=error)
 
-            user_data = session.get('user')
-            if session.get('user') in ['',None]:
-                session['user'] = request.form['name']
-                user_data = session['user']
-            flash('Login succeed')                
-            return render_template('main.html', user=user_data)
-api = API()
+#             user_data = session.get('user')
+#             if session.get('user') in ['',None]:
+#                 session['user'] = request.form['name']
+#                 user_data = session['user']
+#             flash('Login succeed')                
+#             return render_template('main.html', user=user_data)
+# api = API()
 
 app.add_url_rule('/','home',home)
-app.add_url_rule('/login','login',api.login, methods=['GET','POST'])
-app.add_url_rule('/logout','logout', api.logout, methods=['GET'])
-app.add_url_rule('/logout','logout', api.logout, methods=['POST'])
-app.add_url_rule('/hello/','hello',api.hello)
-app.add_url_rule('/hello/<int:revNo>','hello',api.hello)
+# app.add_url_rule('/login','login',api.login, methods=['GET','POST'])
+# app.add_url_rule('/logout','logout', api.logout, methods=['GET'])
+# app.add_url_rule('/logout','logout', api.logout, methods=['POST'])
+# app.add_url_rule('/hello/','hello',api.hello)
+# app.add_url_rule('/hello/<int:revNo>','hello',api.hello)
 if __name__ == "__main__":
     app.secret_key='welcome'
     app.run(debug=False, host='0.0.0.0', port=5000)
